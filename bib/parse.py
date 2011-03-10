89372d2c2660f85d540f137752f8959150e369ea
Sainath Vellal <svellal@gmail.com>
2011-03-10 09:52:43 +0530

    Silence the noise made by Anushsh and Suhel.

--- bib/parse.py
+++ bib/parse.py
@@ -1,11 +1,8 @@
 import re,pdb,codecs
-import simplejson
 import pymongo
 from pymongo import Connection
-from pymongo import son
 
 dic = {}	 
-ll = []
 def parse():
 	text = codecs.open('SouthAsianOrnithology.txt').read()
 	para  = text.split('ER  -')
@@ -25,10 +22,7 @@ def parse():
 				if lst.has_key(k):
 					lst[k].append(val)
 				else:
-					if k == 'KW':
-						lst[k] = val
-					else:
-						lst[k] = [val]
+					lst[k] = [val]
 				lkey = k
 			else:
 				try:
@@ -45,31 +39,20 @@ def parse():
 			for ke,va in lst.items():
 				if len(va) == 1:
 					lst[ke] = va[0]
-				else:
-					try:
-						lst[ke] = '::'.join(va)
-					except:
-						pdb.set_trace()
 			v = lst['ID'].strip()
 			dic[v] = lst
 	
 	i = 0
-	keys = [[] for i in range(10)]
-	dics = [{} for i in range(10)]
 	vals = [[] for i in range(10)]
-	for k,v in dic.items():
-		keys[i%10].append(k)
+	for v in dic.values():
 		vals[i%10].append(v)
 		i += 1
-	for i in range(10):
-		dics[i] = dict(zip(keys[i],vals[i]))
 
 
 	conn = Connection()
 	db = conn['test']
 	for i in range(10):
 		db['coll_%s'%i].insert(vals[i])
-	pdb.set_trace()
 
 if __name__ == "__main__":
 	parse()
